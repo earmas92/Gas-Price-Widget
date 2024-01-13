73a4e0cd7a3c745f9cd21113c1472b55541d6428
PabasaraDilshan <dilshanpabasara@gmail.com>
2024-01-13 13:30:15 +0530

    fixes

--- index.js
+++ index.js
@@ -18,10 +18,23 @@ app.post('/gasbuddy', async (req, res) => {
 
     var myHeaders = new Headers();
     myHeaders.append("authority", "www.gasbuddy.com");
-    // Add other headers as needed
+myHeaders.append("accept", "*/*");
+myHeaders.append("accept-language", "en-US,en;q=0.9");
+myHeaders.append("apollo-require-preflight", "true");
+myHeaders.append("content-type", "application/json");
+myHeaders.append("gbcsrf", "1.ZZjgVN9S4RtbF6zO");
+myHeaders.append("origin", "https://www.gasbuddy.com");
+myHeaders.append("referer", "https://www.gasbuddy.com/home?fuel=1&method=all&maxAge=0&lat=25.7758819&lng=-80.3768659");
+myHeaders.append("sec-ch-ua", "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"");
+myHeaders.append("sec-ch-ua-mobile", "?0");
+myHeaders.append("sec-ch-ua-platform", "\"macOS\"");
+myHeaders.append("sec-fetch-dest", "empty");
+myHeaders.append("sec-fetch-mode", "cors");
+myHeaders.append("sec-fetch-site", "same-origin");
+myHeaders.append("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
 
     var graphql = JSON.stringify({
-      query: "query LocationBySearchTerm($brandId: Int, $cursor: String, $fuel: Int, $lat: Float, $lng: Float, $maxAge: Int, $search: String) {\n  locationBySearchTerm(lat: $lat, lng: $lng, search: $search) {\n    countryCode\n    displayName\n    latitude\n    longitude\n    regionCode\n    stations(\n      brandId: $brandId\n      cursor: $cursor\n      fuel: $fuel\n      lat: $lat\n      lng: $lng\n      maxAge: $maxAge\n    ) {\n      count\n      cursor {\n        next\n        __typename\n      }\n      results {\n        // ... (rest of the query)\n      }\n      __typename\n    }\n    // ... (rest of the query)\n  }\n}",
+      query: "query LocationBySearchTerm($brandId: Int, $cursor: String, $fuel: Int, $lat: Float, $lng: Float, $maxAge: Int, $search: String) {\n  locationBySearchTerm(lat: $lat, lng: $lng, search: $search) {\n    countryCode\n    displayName\n    latitude\n    longitude\n    regionCode\n    stations(\n      brandId: $brandId\n      cursor: $cursor\n      fuel: $fuel\n      lat: $lat\n      lng: $lng\n      maxAge: $maxAge\n    ) {\n      count\n      cursor {\n        next\n        __typename\n      }\n      results {\n        address {\n          country\n          line1\n          line2\n          locality\n          postalCode\n          region\n          __typename\n        }\n        badges {\n          badgeId\n          callToAction\n          campaignId\n          clickTrackingUrl\n          description\n          detailsImageUrl\n          detailsImpressionTrackingUrls\n          imageUrl\n          impressionTrackingUrls\n          targetUrl\n          title\n          __typename\n        }\n        brandings {\n          brandId\n          brandingType\n          __typename\n        }\n        brands {\n          brandId\n          imageUrl\n          name\n          __typename\n        }\n        distance\n        emergencyStatus {\n          hasDiesel {\n            nickname\n            reportStatus\n            updateDate\n            __typename\n          }\n          hasGas {\n            nickname\n            reportStatus\n            updateDate\n            __typename\n          }\n          hasPower {\n            nickname\n            reportStatus\n            updateDate\n            __typename\n          }\n          __typename\n        }\n        enterprise\n        fuels\n        id\n        name\n        offers {\n          discounts {\n            grades\n            highlight\n            pwgbDiscount\n            receiptDiscount\n            __typename\n          }\n          highlight\n          id\n          types\n          use\n          __typename\n        }\n        payStatus {\n          isPayAvailable\n          __typename\n        }\n        prices {\n          cash {\n            nickname\n            postedTime\n            price\n            formattedPrice\n            __typename\n          }\n          credit {\n            nickname\n            postedTime\n            price\n            formattedPrice\n            __typename\n          }\n          discount\n          fuelProduct\n          __typename\n        }\n        priceUnit\n        ratingsCount\n        starRating\n        __typename\n      }\n      __typename\n    }\n    trends {\n      areaName\n      country\n      today\n      todayLow\n      trend\n      __typename\n    }\n    __typename\n  }\n}",
       variables: req.body
     });
 
